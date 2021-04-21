124891776f2ba19ddb8403e4b9a5e16afc15f49f
Scott Heng <scottdavidheng@gmail.com>
2021-04-21 16:58:55 -0400

    pushing code to see if model can load

--- sightly.js
+++ sightly.js
@@ -7,8 +7,14 @@ Filter process:
 
 const video = document.getElementById("webcam");
 
+const width = video.videoWidth;
+const height = video.videoHeight;
+
 var canvas_bp = document.getElementById("canvas-bp");
 var canvas_bp_ctx = canvas_bp.getContext("2d");
+
+canvas_bp_ctx.width = width;
+canvas_bp_ctx.height = height;
 
 
 /*
@@ -26,77 +32,89 @@ const bodyPixProperties = {
 const segmentationProperties = {
   flipHorizontal: false,
   internalResolution: "high",
-  segmentationThreshold: 0.9,
+  segmentationThreshold: 0.5,
   scoreThreshold: 0.2
 };
+
 
 /*
 MODEL IMPLEMENTATION
 */
 
-var ready = true;
-var model = undefined;
+var ready = false;
 
-bodyPix.load(bodyPixProperties).then(function (model_bp) {
-  model = model_bp;
-});
+const loadBodyPixModel = async () => {
+  const model = await bodyPix.load(bodyPixProperties);
+  console.log("Body-Pix model loaded.");
+  ready = true;
+  return model;
+};
 
-// function filter(model) {
-//   if (ready) {
-//     sourceCanvas_ctx.drawImage(video, 0, 0);
-//     ready = false;
+const model = loadBodyPixModel();
 
-//     var segmentation = model.segmentPerson(sourceCanvas, segmentationProperties);
-
-//     //copy-pasted
-//     const coloredPartImage = bodyPix.toMask(segmentation);
-//     const opacity = 0.7;
-//     const flipHorizontal = false;
-//     const maskBlurAmount = 0;
-//     const canvas = document.getElementById("canvas");
-//     // Draw the mask image on top of the original image onto a canvas.
-//     // The colored part image will be drawn semi-transparent, with an opacity of
-//     // 0.7, allowing for the original image to be visible under.
-//     var mask_imagedata = bodyPix.drawMask(
-//       canvas,
-//       img,
-//       coloredPartImage,
-//       opacity,
-//       maskBlurAmount,
-//       flipHorizontal
-//     );
-
-//     processFrame(webcamCanvas, mask_imagedata);
-//     ready = true;
-//   }
-
-//   window.requestAnimationFrame(filter(model));
-// }
-
-function paintToCanvas() {
-  setInterval(() =>{ canvas_bp_ctx.drawImage(video,0,0,canvas_bp.width, canvas_bp.height);
-  }, 16);
+function filter() {
+  if (ready) {
+    // var frame = getNextFrame();
+    // var personSegment = frameSegmentation(frame);
+    // updateFeed(personSegment, frame);
+    canvas_bp_ctx.drawImage(video,0,0,canvas_bp.width, canvas_bp.height)
   }
+}
+
+function filterInterval() {
+  setInterval(() =>{ filter();
+  }, 16);
+}
+
+//gets the nextframe in the webcam
+function getNextFrame() {
+  return sourceCanvas_ctx.drawImage(video,0,0,canvas_bp.width,canvas_bp.height);
+};
+
+const frameSegmentation = async () => {
+  var segmentation = await model.segmentPerson(sourceCanvas, segmentationProperties);
+  return segmentation;
+};
+
+const maskFrame = async (personSegment) => {
+  const coloredPartImage = await bodyPix.toMask(personSegment);
+  return coloredPartImage;
+};
+
+const drawMask = async(coloredPartImage, opacity, maskBlueAmount, flipHorizontal) => {
+    bodyPix.drawMask(canvas_bp, sourceCanvas, coloredPartImage, opacity, maskBlueAmount, flipHorizontal);
+    // canvas_bp_ctx.drawImage(video,0,0,canvas_bp.width, canvas_bp.height);
+    console.log("masking completed");
+};
+
+function updateFeed(personSegment, frame) {
+  const coloredPartImage = maskFrame(personSegment);
+  const opacity = 0.7;
+  const flipHorizontal = false;
+  const maskBlurAmount = 0;
+
+  drawMask(coloredPartImage,opacity, flipHorizontal, maskBlurAmount);
+}
+
+
+//trying a different approach
+async function loadAndPredict() {
+  const net = await bodyPix.load()
+}
 
 // main
 
-//starts all webcam streams
+//starts webcam stream
 if (navigator.mediaDevices.getUserMedia) {
   const user_params = {
     video: true,
-    // video_bp: true
   };
 
   navigator.mediaDevices
     .getUserMedia(user_params)
     .then(function (stream) {
       video.srcObject = stream;
-
-    //   video_bp.addEventListener("loadeddata", filter(model));
-      // setInterval(() =>{ 
-      //   canvas_bp_ctx.drawImage(video,0,0,canvas_bp.width, canvas_bp.height);
-      // }, 16);
-      paintToCanvas();
+      video.addEventListener("loaddeddata", filterInterval());
     })
     .catch(function (problem) {
       console.log("An Error Occurred");
@@ -109,3 +127,4 @@ var sourceCanvas_ctx = sourceCanvas.getContext("2d");
 
 // Remember considering time taken for model to load
 
+ 
