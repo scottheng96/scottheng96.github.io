90f7cbd4e19ba48d83110d3477baedb4a65ef9e6
Scott Heng <scottdavidheng@gmail.com>
2021-09-13 11:29:02 -0400

    used formspree.io

--- form.php
+++
@@ -1,7 +0,0 @@
-<?php
-// $toemail = "hengnotary@gmail.com";
-// $subject = "Query from Heng Notary Website from: " . $_POST["contact_name"] . ", " . $_POST["contact_email"]  ;
-// $message = $subject . "/n" . wordwrap($_POST["contact_message"]);
-// mail($toemail,$subject,$message);
-echo "Hello World!";
-?>
